4f6a00233f466ecd5668e816bf2b6c49cd5477ab
iantal <antal.micky@yahoo.com>
2020-10-05 00:32:22 +0300

    Base64 decoding of grpc request data

--- maven-parser/src/main/kotlin/io/mavenparser/mavenparser/grpc/ParseService.kt
+++ maven-parser/src/main/kotlin/io/mavenparser/mavenparser/grpc/ParseService.kt
@@ -1,24 +1,53 @@
 package io.mavenparser.mavenparser.grpc
 
+import com.google.common.io.BaseEncoding.base64
+import io.mavenparser.mavenparser.Library
 import io.mavenparser.mavenparser.MavenParseServiceGrpcKt
 import io.mavenparser.mavenparser.ParseRequest
 import io.mavenparser.mavenparser.ParseResponse
 import io.mavenparser.mavenparser.Project
+import io.mavenparser.mavenparser.core.Parser
+import io.mavenparser.mavenparser.core.StateManager
+import java.lang.Byte.decode
+import java.nio.charset.StandardCharsets
+import java.util.*
+
 
 class ParseService : MavenParseServiceGrpcKt.MavenParseServiceCoroutineImplBase() {
     override suspend fun parse(request: ParseRequest): ParseResponse {
         println("received maven parse request for ${request.data}")
 
-        val libraries = listOf("abs", "ade")
+        val decodedData = decodeBase64(request.data)
 
-        val projects = listOf(Project.newBuilder()
-                .setName("library1")
-                .addAllLibraries(libraries)
-                .build()
-        )
+        val parser = Parser(StateManager())
+        val parsedData = parser.parse(decodedData)
+
+        val projects = mutableListOf<Project>()
+        for (project in parsedData) {
+            val libs = mutableListOf<Library>()
+            for (library in project?.libraries!!) {
+                libs.add(Library.newBuilder()
+                        .setName(library.name)
+                        .setType(library.type.toString().toLowerCase())
+                        .setScope(library.scope.toString().toLowerCase())
+                        .build())
+            }
+            projects.add(Project.newBuilder()
+                    .setName(project.name)
+                    .addAllLibraries(libs)
+                    .build())
+        }
 
         return ParseResponse.newBuilder()
                 .addAllProjects(projects)
                 .build()
+    }
+
+    fun decodeBase64(data: String) : String{
+        val decoder = Base64.getDecoder()
+        val decoded = decoder.decode(data)
+        val decodedStr = String(decoded, Charsets.UTF_8)
+        println(decodedStr)
+        return decodedStr
     }
 }
